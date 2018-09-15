709b5215e3ef05622758ff7698db080f358c6425
herbertchen1 <2648204787@qq.com>
2018-09-15 21:29:06 +0800

    on solution time ex, for Container With Most Water the only way higher is change low side to be higher

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,17 +1,35 @@
 class Solution:
 	#O(n2) solution
+	# def maxArea(self, height):
+	# 	"""
+	# 	:type height: List[int]
+	# 	:rtype: int
+	# 	"""
+	# 	maxArea=-1
+	# 	for i in range(len(height)-1):
+	# 		for j in range(i+1,len(height)):
+	# 			Area=self.calculateIJ(i,j,height)
+	# 			if Area>maxArea:
+	# 				maxArea=Area
+	# 	return maxArea
 	def maxArea(self, height):
 		"""
 		:type height: List[int]
 		:rtype: int
 		"""
-		maxArea=-1
-		for i in range(len(height)-1):
-			for j in range(i+1,len(height)):
-				Area=self.calculateIJ(i,j,height)
-				if Area>maxArea:
-					maxArea=Area
-		return maxArea
+		i = 0
+		j = len(height) - 1
+		maxarea = 0
+		while (i < j):
+			if height[i] < height[j]:
+				area = height[i] * (j - i)
+				if area > maxarea: maxarea = area
+				i += 1
+			else:
+				area = height[j] * (j - i)
+				if area > maxarea: maxarea = area
+				j -= 1
+		return maxarea
 
 	def calculateIJ(self,i,j,height):
 		return abs(i-j)*min(height[i],height[j])
